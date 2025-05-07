74c1b2b34a0994acca1d6c3e3b5f6cf69433f22d
Pat Tul <patchatulyakul@gmail.com>
2025-05-07 15:34:35 -0400

    1st commit-initiate landing page, font, metatag

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/icons/Icons1.svg"/>
+        <h1>Patcharalak Tulyakul</h1>
+        <p>Hello! My name is Patcharalak Tulyakul. I'm a Computer Science student.</p>
       </header>
     </div>
   );
