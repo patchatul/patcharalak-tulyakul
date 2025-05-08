0b214852766ec5ed89b823b14acb1f46cd0a1b7d
Pat Tul <patchatulyakul@gmail.com>
2025-05-08 15:51:42 -0400

    3 add icons&pics to public, Certificates

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <nav>
-          <img src="Icons1.svg"/>
+          <h1>Patcharalak Tulyakul</h1>
+          <img src="/icons/Icon1.svg" alt="menu"/>
         </nav>
-        <h1>Patcharalak Tulyakul</h1>
-        <p>Hello! My name is Patcharalak Tulyakul. I'm a Computer Science student.</p>
+        <div>
+          <img src="/icons/Pat.jpg" alt="Picture of me"/>
+          <p>Hello! My name is Patcharalak Tulyakul. I'm a Computer Science student.</p>
+        </div>
+        
         <div>
           <a href="https://github.com/patchatul">github</a>
           <a href="https://www.linkedin.com/in/patcharalak-tulyakul/">linkedin</a>
--- src/Certificates.js
+++ src/Certificates.js
@@ -5,15 +5,28 @@ function Certificates() {
     <div className="App">
       <header className="App-header">
         <h1>Certificates</h1>
-        <p>Meta Front-End Developer</p>
-        <a href="https://www.coursera.org/account/accomplishments/professional-cert/34FVJVRS4DKP"><img src="https://www.coursera.org/account/accomplishments/specialization/certificate/34FVJVRS4DKP"/></a>
-        <p>Projects and skills: </p>
-        <p>Responsive Web Design-freeCodeCamp</p>
-        <a href="https://www.freecodecamp.org/certification/fcc79225faf-3524-4849-a338-b14c63eb50e9/javascript-algorithms-and-data-structures"><img scr=""/></a>
-        <p>Projects and skills: </p>
-        <p>Legacy JavaScript Algorithms and Data Structures-freeCodeCamp</p>
-        <a href="https://www.freecodecamp.org/certification/fcc79225faf-3524-4849-a338-b14c63eb50e9/responsive-web-design"><img scr=""/></a>
-        <p>Projects and skills: </p>
+        <div>
+          <p>Meta Front-End Developer</p>
+          <a href="https://www.coursera.org/account/accomplishments/professional-cert/34FVJVRS4DKP">
+            <img src="/icons/MetaFrontEndDeveloperCoursera.jpg" alt="Meta Fronf-End Developer Coursera Certificate"/>
+          </a>
+          <p>Projects and skills: HTML, CSS, JavaScript, React, Bootstrap CSS Framework, Web Development, UI/UX design, User Experience, Algorithms, Version Control</p>
+        </div>
+        <div>
+          <p>Responsive Web Design-freeCodeCamp</p>
+          <a href="https://www.freecodecamp.org/certification/fcc79225faf-3524-4849-a338-b14c63eb50e9/javascript-algorithms-and-data-structures">
+            <img src="/icons/ResponsiveWebfreeCodeCamp.png" alt="Responsive Web Design freeCodeCamp Certificate"/>
+          </a>
+          <p>Projects and skills: </p>
+        </div>
+        <div>
+          <p>Legacy JavaScript Algorithms and Data Structures-freeCodeCamp</p>
+          <a href="https://www.freecodecamp.org/certification/fcc79225faf-3524-4849-a338-b14c63eb50e9/responsive-web-design">
+            <img src="/icons/JavaScriptfreeCodeCamp.png" alt="Legacy JavaScript Algorithms and Data Structures freeCodeCamp Certificate"/>
+          </a>
+          <p>Projects and skills: </p>
+        </div>
+        
       </header>
     </div>
   );
