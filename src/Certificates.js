cd412598e99b03ef94c162e2f5cc5047f3d622e8
Pat Tul <patchatulyakul@gmail.com>
2025-05-22 21:46:03 -0400

    11 add Certificates page

--- src/Certificates.js
+++ src/Certificates.js
@@ -5,25 +5,25 @@ function Certificates() {
     <div className="Certificates">
       <h2>Certificates</h2>
         <div>
-          <p>Meta Front-End Developer</p>
+          <h3>Meta Front-End Developer</h3>
           <a href="https://www.coursera.org/account/accomplishments/professional-cert/34FVJVRS4DKP">
             <img src="/icons/MetaFrontEndDeveloperCoursera.jpg" alt="Meta Fronf-End Developer Coursera Certificate"/>
           </a>
-          <p>Projects and skills: HTML, CSS, JavaScript, React, Bootstrap CSS Framework, Web Development, UI/UX design, User Experience, Algorithms, Version Control</p>
+          <p className="Certificates-text">Skills & Knowledge:<br/>HTML, CSS, JavaScript, React, Bootstrap, Web Development, UI/UX design, Version Control</p>
         </div>
         <div>
-          <p>Responsive Web Design-freeCodeCamp</p>
-          <a href="https://www.freecodecamp.org/certification/fcc79225faf-3524-4849-a338-b14c63eb50e9/javascript-algorithms-and-data-structures">
+          <h3>Responsive Web Design-freeCodeCamp</h3>
+          <a href="https://www.freecodecamp.org/certification/fcc79225faf-3524-4849-a338-b14c63eb50e9/responsive-web-design">
             <img src="/icons/ResponsiveWebfreeCodeCamp.png" alt="Responsive Web Design freeCodeCamp Certificate"/>
           </a>
-          <p>Projects and skills: </p>
+          <p className="Certificates-text">Skills & Knowledge:<br/>Responsive Web Design, HTML, CSS</p>
         </div>
         <div>
-          <p>Legacy JavaScript Algorithms and Data Structures-freeCodeCamp</p>
+          <h3>Legacy JavaScript Algorithms and Data Structures-freeCodeCamp</h3>
           <a href="https://www.freecodecamp.org/certification/fcc79225faf-3524-4849-a338-b14c63eb50e9/responsive-web-design">
             <img src="/icons/JavaScriptfreeCodeCamp.png" alt="Legacy JavaScript Algorithms and Data Structures freeCodeCamp Certificate"/>
           </a>
-          <p>Projects and skills: </p>
+          <p className="Certificates-text">Skills & Knowledge:<br/>Algorithms, Data Structures, JavaScript, Object Oriented Programming, Functional Programming</p>
         </div>
     </div>
   );
--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,10 @@ function Projects(){
     return (
         <div className="Projects">
             <h2>Projects</h2>
+            <h3>Little Lemon Website</h3>
+            <img alt="Little Lemon website"></img>
+            <a href="">visit github repo</a>
+            <p>Responsive website using HTML, CSS, Bootstrap, Javascript, React</p>
         </div>
       );    
 }
